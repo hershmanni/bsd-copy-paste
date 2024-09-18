719b297f66c767263f7e8fb45008182b77a44be4
Nick Hershman <nick_hershman@beaverton.k12.or.us>
2024-09-18 08:48:38 -0700

    Ignore helpers/* when creating release.

--- get-zip.sh
+++ get-zip.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 # -x excludes 
-zip -r "../bsd-copy-paste-${1}.zip" * -x get-zip.sh .git/* dev-store/* dev-store/ .git/
+zip -r "../bsd-copy-paste-${1}.zip" * -x get-zip.sh .git/* dev-store/* dev-store/ .git/ helpers/*
