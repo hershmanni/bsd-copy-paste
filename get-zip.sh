abf3f28579d19c84dcdf85736bbda191bcb8776d
Nick Hershman <nick.hershman@gmail.com>
2026-02-10 01:48:59 -0800

    Update bundler.

--- get-zip.sh
+++ get-zip.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 
-# -x excludes 
-zip -r "../bsd-copy-paste-${1}.zip" * -x get-zip.sh .git/* dev-store/* dev-store/ .git/ helpers/*
+# -x excludes (quoted to avoid zsh "no matches found" glob errors)
+zip -r "../bsd-copy-paste-${1}.zip" . \
+  -x "get-zip.sh" ".git/*" ".git/" "dev-store/*" "dev-store/" "helpers/*" ".hidden/*" ".hidden/" "*.DS_Store"
